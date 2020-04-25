4e5769402daa7d313668978f2b31656bfbee8dfc
Pedro Barros <pedro.barros@herospark.com>
2020-04-25 02:34:38 -0300

    first commit

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,45 @@
+import request from 'supertest';
+import bcrypt from 'bcryptjs';
+import app from '../../src/app';
+import truncate from '../util/truncate';
+import factory from '../factory';
+
+describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('shoudl encrypt user password when new user created', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBeTruthy();
+  });
+
+  it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User'); // get unique user for test
+
+    await request(app)
+      .post('/users')
+      .send(user);
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(response.status).toBe(400);
+  });
+});
